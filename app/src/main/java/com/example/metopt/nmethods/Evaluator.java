9aa64e99ae00b664d95beb7a85ec1bf40d5efb2a
MariaDziuba <62502938+MariaDziuba@users.noreply.github.com>
2021-04-17 01:42:44 +0500

    Added doc

--- app/src/main/java/com/example/metopt/nmethods/Evaluator.java
+++ app/src/main/java/com/example/metopt/nmethods/Evaluator.java
@@ -1,15 +1,15 @@
 package com.example.metopt.nmethods;
 
-import com.example.metopt.methods.BrentsMethods;
-import com.example.metopt.methods.DichotomyMethod;
-import com.example.metopt.methods.FibonacciMethod;
-
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Вычисляет минимум квадратичных функций, используя три метода:
+ * градиентного спуска, наискорейшего спуска, сопряженных градиентов
+ */
 public class Evaluator {
 
     Set<AbstractNMethod> methods;
@@ -17,24 +17,27 @@ public class Evaluator {
     public Evaluator(QuadraticFunction fun) {
         methods = new HashSet<>(List.of(
                 new GradientMethod(fun, "Gradient descent"),
-                new GradientOpt(fun, "Fast gradient descent"),
+                new FastGradientMethod(fun, "Fast gradient descent"),
                 new ConjugateGradientMethod(fun, "Conjugate gradient descent")));
     }
 
-    private void print(AbstractNMethod method, PrintWriter pw, QuadraticFunction fun) {
+    private void print(AbstractNMethod method, PrintWriter pw) {
         pw.println(method.name + " method");
         pw.printf("%.4f%n", method.computeMin());
     }
 
-    public void evaluate(QuadraticFunction fun) {
+    public void evaluate() {
         PrintWriter pw = new PrintWriter(System.out);
         for (AbstractNMethod method : methods) {
-            print(method, pw, fun);
+            print(method, pw);
         }
         pw.close();
 
     }
 
+    /**
+     * Генерирует типовую квадратичную функцию для тестирования программы
+     */
     private static QuadraticFunction getQuadraticFunction() {
         // 20x^2 + y^2 -7x + 3y + 2
         List<List<Double>> a = Arrays.asList(Arrays.asList(40.0, 0.0), Arrays.asList(0.0, 2.0));
@@ -46,6 +49,6 @@ public class Evaluator {
     public static void main(String[] args) {
         QuadraticFunction fun = getQuadraticFunction();
         Evaluator evaluator = new Evaluator(fun);
-        evaluator.evaluate(fun);
+        evaluator.evaluate();
     }
 }
