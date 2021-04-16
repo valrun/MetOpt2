e5381c8c1ba9e8939e6edb3b08ece0eb7332ad24
MariaDziuba <62502938+MariaDziuba@users.noreply.github.com>
2021-04-17 01:41:12 +0500

    Added doc

--- app/src/main/java/com/example/metopt/nmethods/AbstractOneDimMethod.java
+++ app/src/main/java/com/example/metopt/nmethods/AbstractOneDimMethod.java
@@ -1,5 +1,8 @@
 package com.example.metopt.nmethods;
 
+/**
+ * Абстрактный класс для методов одномерной оптимизации
+ */
 public abstract class AbstractOneDimMethod {
     abstract double findMin(double l, double r, double EPS);
 }
