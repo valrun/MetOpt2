db26f707d78a1c9b3c448f158bd0391af9ad9304
MariaDziuba <62502938+MariaDziuba@users.noreply.github.com>
2021-04-17 01:46:50 +0500

    Added doc and description

---
+++ app/src/main/java/com/example/metopt/nmethods/FastGradientMethod.java
@@ -0,0 +1,41 @@
+package com.example.metopt.nmethods;
+
+import java.util.function.Function;
+
+/**
+ * Метод наискорейшего спуска основывается на выборе шага из следующего
+ * соображения. Из точки  x^{[k]} будем двигаться в направлении антиградиента
+ * до тех пор пока не достигнем минимума функции f на этом направлении,
+ * т. е. на луче
+ *
+ * Другими словами, alpha^{[k]} выбирается так, чтобы следующая итерация
+ * была точкой минимума функции f на луче L. Заметим, кстати, что в этом
+ * методе направления соседних шагов ортогональны.
+ *
+ * Метод наискорейшего спуска требует решения на каждом шаге задачи одномерной
+ * оптимизации. Практика показывает, что этот метод часто требует меньшего
+ * числа операций, чем градиентный метод с постоянным шагом.
+ */
+
+public class FastGradientMethod extends AbstractNMethod {
+
+    Function<Double, Vector> fun;
+
+    public FastGradientMethod(QuadraticFunction fun, String name) {
+        super(fun, name);
+    }
+
+    @Override
+    Value<Vector, Double> iterate(Value<Vector,Double> x) {
+        fun = t -> x.getVal().add(grad.multiply(-t));
+        AbstractOneDimMethod oneDimMethod = new GoldenRatioMethod(fun.andThen(getFunction()));
+        double alpha = oneDimMethod.findMin(0.0, 1.0, EPS / 100);
+        return new Value<>(fun.apply(alpha), getFunction());
+    }
+
+    @Override
+    boolean cycleCondition() {
+        grad = getFunction().getGrad(x.getVal());
+        return !(grad.norm() < EPS);
+    }
+}
